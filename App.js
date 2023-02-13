336e3805f61dd75dde8d335977d6932919f9c688
IsraelBaruque <israekbaruque992@gmail.com>
2023-02-13 13:31:04 -0300

    start project

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.p}>Bom dia, Buenos dias, Good morning</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  p: {
+    fontSize: 20,
+    color: 'red',
+  }
 });
